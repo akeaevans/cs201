7d04210813f773fc0f7eb88b3c9bbffb441e8825
Glenn G. Chappell <ggchappell@alaska.edu>
2018-01-21 19:40:42 -0900

    Add hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+// hello.cpp
+// Glenn G. Chappell
+// 19 Jan 2018
+//
+// For CS 201 Spring 2018
+// Hello-World Example
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Hello, world!" << endl;
+    cout << "I hope you're having ";
+    cout << "a great day!" << endl;
+
+    return 0;
+}
+
