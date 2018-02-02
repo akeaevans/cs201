6947e673e58ddb5a6b7066179aa328c75984c573
Glenn G. Chappell <ggchappell@alaska.edu>
2018-02-01 23:09:50 -0900

    Add intio.h/.cpp

---
+++ intio.h
@@ -0,0 +1,37 @@
+// intio.h
+// Glenn G. Chappell
+// 1 Feb 2018
+//
+// For CS 201 Spring 2018
+// Header for Integer I/O Package
+
+#ifndef FILE_NUMIO_H_INCLUDED
+#define FILE_NUMIO_H_INCLUDED
+
+
+// getInt
+// Get an integer from a typed line of text; repeat until successful.
+// Does no error checking on cin, so avoid using for file input.
+//
+// Sample usage:
+//   cout << "Type your favorite number: ";
+//   int favnum = getInt();
+//
+int getInt();
+
+
+// putInt
+// Print the given integer value to cout. At least minchars characters
+// are printed. If the number requires fewer characters, then the output
+// is padded with leading blanks.
+//
+// Sample usage:
+//   cout << "Your favorite number: ";
+//   putInt(favnum, 10);  // Print favnum, using at least 10 characters
+//
+void putInt(int value,      // Value to output
+            int minchars);  // Minimum number of characters to output
+
+
+#endif //#ifndef FILE_NUMIO_H_INCLUDED
+
