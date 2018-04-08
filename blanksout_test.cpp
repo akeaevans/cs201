6454483bd47f66a0d146232032c79901f70c7333
Glenn G. Chappell <ggchappell@alaska.edu>
2018-04-07 19:01:02 -0800

    Add blanksout_test.cpp

---
+++ blanksout_test.cpp
@@ -0,0 +1,137 @@
+// blanksout_test.cpp
+// Glenn G. Chappell
+// 7 Apr 2018
+//
+// For CS 201, Spring 2018
+// Test program for function blanksOut
+// Used in Assignment 5, Exercise B
+
+#include "blanksout.h"  // For function blanksOut
+#include "blanksout.h"  // Double inclusion check
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include <map>
+using std::map;
+#include <string>
+using std::string;
+
+
+// doTest
+// Do a single test of function blanksOut. Given are the parameter for
+// blanksOut, the expected return value, and a bool indicating whether
+// all tests have passed so far. The test is done. Results are printed
+// to cout. If the test failed, then allPassed is set to false.
+void doTest(const map<int, string> & data,      // Test data
+            const map<int, string> & expected,  // Expected return value
+            bool & allpassed)              // Set to false if test fails
+{
+    static int testnum = 0;
+    ++testnum;
+    auto result = data;
+    blanksOut(result);
+    cout << "Test #" << testnum << ": ";
+
+    // Did test pass?
+    if (result == expected)
+    {
+        cout << "passed" << endl;
+        return;
+    }
+
+    // Test failed
+    allpassed = false;
+    cout << "FAILED **********" << endl;
+}
+
+
+// Main program
+// Do tests of function blanksOut. Print results to cout.
+int main()
+{
+    bool allpassed = true;  // Have all tests so far passed?
+    cout << "Test Suite for function blanksOut" << endl;
+    cout << endl;
+
+    // Tests from assignment description
+
+    {
+    map<int, string> m {
+        { 33, "  x  y   " },
+        { 51, "zzz" },
+        { 40, "    aaa" }
+    };
+    map<int, string> mr {
+        { 33, "x  y" },
+        { 51, "zzz" },
+        { 40, "aaa" }
+    };
+    doTest(m, mr, allpassed);  // Test #1
+    }
+
+    // Other tests
+
+    {
+    map<int, string> m;
+    map<int, string> mr;
+    doTest(m, mr, allpassed);  // Test #2
+    }
+
+    {
+    map<int, string> m {
+        { 1, "Z" }
+    };
+    map<int, string> mr {
+        { 1, "Z" }
+    };
+    doTest(m, mr, allpassed);  // Test #3
+    }
+
+    {
+    map<int, string> m {
+        { 1, "Z            " }
+    };
+    map<int, string> mr {
+        { 1, "Z" }
+    };
+    doTest(m, mr, allpassed);  // Test #4
+    }
+
+    {
+    map<int, string> m {
+        { 1, "                                 " }
+    };
+    map<int, string> mr {
+        { 1, "" }
+    };
+    doTest(m, mr, allpassed);  // Test #5
+    }
+
+    {
+    map<int, string> m {
+        { -17000, "" }
+    };
+    map<int, string> mr {
+        { -17000, "" }
+    };
+    doTest(m, mr, allpassed);  // Test #6
+    }
+
+    // Print results
+    cout << endl;
+    if (allpassed)
+    {
+        cout << "All tests passed" << endl;
+    }
+    else
+    {
+        cout << "NOT ALL TESTS PASSED ************************" << endl;
+    }
+    cout << endl;
+
+    // Quit when user hits Enter
+    cout << "Press ENTER to quit ";
+    while (cin.get() != '\n') ;
+}
+
